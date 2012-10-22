1e9808336fbb8d123f1114289911f30fdba227d9
Andrew Pullin <pullin@berkeley.edu>
2012-10-21 18:43:19 -0700

    Working configuration for reading AN0, AN8-AN11 sequentially with ADC1 with DMA.

--- lib/adc_pid.c
+++ lib/adc_pid.c
@@ -10,6 +10,7 @@
 #include "p33Fxxxx.h"
 #include "ports.h"
 
+
 //Functions
 static void adcSetupPeripheral(void);
 //DMA related functions
@@ -18,10 +19,9 @@ void __attribute__((__interrupt__)) _DMA0Interrupt(void);
 
 
 //Variables to store values as they come out of the DMA buffer
-static unsigned int adc_bemfL;
-static unsigned int adc_bemfR;
-static unsigned int adc_battery;
-static unsigned int adc_AN3;
+static unsigned int adc_MotorA, adc_MotorB, adc_MotorC, adc_MotorD; // motors
+static unsigned int adc_AN8, adc_AN9, adc_AN10, adc_AN11;           //also motors
+static unsigned int adc_AN0, adc_Vbatt; //battery
 
 void adcSetup(void){
 	adcSetupPeripheral();
@@ -44,10 +44,10 @@ static void adcSetupPeripheral(void) {
             ADC_SAMP_ON; //sample / hold amplifiers are sampling  (maybe incorrect)
     AD1CON2value = ADC_VREF_AVDD_AVSS & //Vref+ = AVdd , Vref- = AVss
             ADC_SCAN_ON & //Scan through ADC channels
-            ADC_SELECT_CHAN_0 & //Only CH0, will scan through all 5 pins
+            ADC_SELECT_CHAN_0 & //Only CH0, will scan
             ADC_ALT_BUF_OFF & //Use one 16 word buffer
             ADC_ALT_INPUT_OFF & // Alternate between MUXA and MUXB
-            ADC_DMA_ADD_INC_1; //Increment DMA address after each sample
+            ADC_DMA_ADD_INC_4; //Increment DMA address after each sample
     AD1CON3value = ADC_CONV_CLK_SYSTEM & //Use System clock, not internal RC osc
             ADC_CONV_CLK_3Tcy & //Tad = 3 * Tcy TODO: Check this
             ADC_SAMPLE_TIME_1; //Sample Time = 1*Tad
@@ -57,19 +57,16 @@ static void adcSetupPeripheral(void) {
     AD1CHS123value = 0; //Dummy value, CH 1,2,3 unused
 
     AD1CHS0value = ADC_CH0_NEG_SAMPLEA_VREFN & // Sample A, Vref- = AVss = ground
-            ADC_CH0_POS_SAMPLEA_AN0 & // Battery
-            ADC_CH0_POS_SAMPLEA_AN8 & // Motor A
-            ADC_CH0_POS_SAMPLEA_AN9 & // Motor B
-            ADC_CH0_POS_SAMPLEA_AN10& // Motor C
             ADC_CH0_POS_SAMPLEA_AN11; // Motor D
 
 
     AD1CSSHvalue = SCAN_NONE_16_31; //Skip AN16-AN131 for Input Scan, not avail on dsPic33
     //Scan: AN0, AN8, AN9, AN10, AN11
-    AD1CSSLvalue = SCAN_ALL_0_15 & SKIP_SCAN_AN1 & SKIP_SCAN_AN2 &
-            SKIP_SCAN_AN3 & SKIP_SCAN_AN4 & SKIP_SCAN_AN5 &
-            SKIP_SCAN_AN6 & SKIP_SCAN_AN7 & SKIP_SCAN_AN12 &
-            SKIP_SCAN_AN13 & SKIP_SCAN_AN14 & SKIP_SCAN_AN15; //Leaving: AN0, AN8, AN9, AN10, AN11
+    AD1CSSLvalue = SCAN_NONE_0_15 | (1 << 0) //AN0
+                                  | (1 << 8) //AN8
+                                  | (1 << 9) //AN9
+                                  | (1 << 10) //AN10
+                                  | (1 << 11); //AN11
 
     //Set pins to analog inputs; also check init_default.c
     AD1PCFGHvalue = ENABLE_ALL_DIG_16_31; //Shouldn't matter, only AN0-15 on 706A
@@ -80,7 +77,28 @@ static void adcSetupPeripheral(void) {
             ENABLE_AN11_ANA; //Motor D
 
     SetChanADC1(AD1CHS123value, AD1CHS0value);
-    OpenADC1(AD1CON1value, AD1CON2value, AD1CON3value, AD1CON4value, AD1PCFGLvalue, AD1PCFGHvalue, AD1CSSHvalue, AD1CSSLvalue);
+    OpenADC1(AD1CON1value, AD1CON2value, AD1CON3value, AD1CON4value, 
+            AD1PCFGLvalue, AD1PCFGHvalue, AD1CSSHvalue, AD1CSSLvalue);
+
+    //The following is a "patch" to the above settings
+    AD1CON2bits.SMPI = 5-1;  //every 5th sample
+    AD1CON2bits.CHPS = 0;
+    AD1CON1bits.SIMSAM = 0; //overridden
+    AD1CON1bits.ADDMABM = 0;
+    AD1CON4bits.DMABL = 0; //1 word buffer each
+    AD1CON2bits.ALTS = 0;
+    AD1CHS0bits.CH0NA = 0;
+    AD1CHS0bits.CH0SA = 0; //overridden
+    AD1CON2bits.CSCNA = 1;
+    AD1CSSL = 0; //clear it first
+    AD1CSSLbits.CSS0 = 1;
+    AD1CSSLbits.CSS8 = 1;
+    AD1CSSLbits.CSS9 = 1;
+    AD1CSSLbits.CSS10 = 1;
+    AD1CSSLbits.CSS11 = 1;
+
+
+
 
     IFS0bits.AD1IF = 0; // Clear the A/D interrupt flag bit
     IEC0bits.AD1IE = 0; //Disable A/D interrupt
@@ -101,23 +119,48 @@ void __attribute__((interrupt,no_auto_psv)) _ADC1Interrupt(void)
 }
 */
 
+
+//Getters for other modules to access values, by AN pin name
+unsigned int adcGetAN0(){
+	return adc_AN0;
+}
+
+unsigned int adcGetAN8(){
+	return adc_AN8;
+}
+
+unsigned int adcGetAN9(){
+	return adc_AN9;
+}
+
+unsigned int adcGetAN10(){
+	return adc_AN10;
+}
+
+unsigned int adcGetAN11(){
+	return adc_AN11;
+}
+
 //Getters for other modules to access values
-unsigned int adcGetBEMFL(){
-	return adc_bemfL;
+unsigned int adcGetVbatt(){
+	return adc_Vbatt;
 }
 
-unsigned int adcGetBEMFR(){
-	return adc_bemfR;
+unsigned int adcGetMotorA(){
+	return adc_MotorA;
 }
 
-unsigned int adcGetVBatt(){
-	return adc_battery;
+unsigned int adcGetMotorB(){
+	return adc_MotorB;
 }
 
-unsigned int adcGetAN3(){
-	return adc_AN3;
+unsigned int adcGetMotorC(){
+	return adc_MotorC;
 }
 
+unsigned int adcGetMotorD(){
+	return adc_MotorD;
+}
 
 //////////////////////////////////////////////////////////////////////
 ///////////////      DMA Section     /////////////////////////////////
@@ -126,64 +169,68 @@ unsigned int adcGetAN3(){
 #define  SAMP_BUFF_SIZE	 		1		// Size of the input buffer per analog input
 
 //Buffers need special attribute to be in DMA memory space
-static int  BufferA[4][SAMP_BUFF_SIZE] __attribute__((space(dma)));
-static int  BufferB[4][SAMP_BUFF_SIZE] __attribute__((space(dma)));
+static int  BufferA[5][SAMP_BUFF_SIZE] __attribute__((space(dma)));
+static int  BufferB[5][SAMP_BUFF_SIZE] __attribute__((space(dma)));
 
 static unsigned int DmaBuffer = 0;
 
 
-
 /*****************************************************************************
-* Function Name : initDma0
-* Description   : Setup function for DMA0, to read ADC1 into a buffer
-* Parameters    : None
-* Return Value  : None
-*****************************************************************************/
-static void initDma0(void)
-{
-	DMA0CONbits.AMODE = 0;			// Configure DMA for Register Indirect w/ post-increment
-	DMA0CONbits.MODE  = 2;			// Configure DMA for Continuous Ping-Pong mode
-	
-	DMA0PAD=(int)&ADC1BUF0;
-	//DMA0CNT = (SAMP_BUFF_SIZE*2)-1;					
-	DMA0CNT = 3;  //See dsPIC user's manual. 4 analog reads -> DMA0CNT = 4-1 = 3
-	//DMA0CNT = 7;
+ * Function Name : initDma0
+ * Description   : Setup function for DMA0, to read ADC1 into a buffer
+ * Parameters    : None
+ * Return Value  : None
+ *****************************************************************************/
+static void initDma0(void) {
+    DMA0CONbits.AMODE = 0; // Configure DMA for Register Indirect w/ post-increment
+    DMA0CONbits.MODE = 2; // Configure DMA for Continuous Ping-Pong mode
 
-	DMA0REQ=13; //ADC1 requests
+    DMA0PAD = (int) &ADC1BUF0;
+    //DMA0CNT = (SAMP_BUFF_SIZE*2)-1;
+    DMA0CNT = 4; //See dsPIC user's manual. 5 analog reads -> DMA0CNT = 5-1 = 4
 
-	DMA0STA = __builtin_dmaoffset(BufferA);		
-	DMA0STB = __builtin_dmaoffset(BufferB);
+    DMA0REQ = 13; //ADC1 requests
 
-	IFS0bits.DMA0IF = 0;			//Clear the DMA interrupt flag bit
-    IEC0bits.DMA0IE = 1;			//Set the DMA interrupt enable bit
+    DMA0STA = __builtin_dmaoffset(BufferA);
+    DMA0STB = __builtin_dmaoffset(BufferB);
 
-	DMA0CONbits.CHEN=1;
+    IFS0bits.DMA0IF = 0; //Clear the DMA interrupt flag bit
+    IEC0bits.DMA0IE = 1; //Set the DMA interrupt enable bit
+
+    DMA0CONbits.CHEN = 1;
 }
-
 /*****************************************************************************
-* Function Name : _DMA0Interrupt
-* Description   : Interrupt hander for DMA0 , associated with ADC1 here.
-				  Motor BEMF vales are set through setter functions.
-* Parameters    : None
-* Return Value  : None
-*****************************************************************************/
-void __attribute__((interrupt, no_auto_psv)) _DMA0Interrupt(void)
-{
-	if(DmaBuffer==0) {
-		adc_bemfL = 	BufferA[0][0];	//AN11
-		adc_battery = 	BufferA[1][0];	//AN0
-		adc_bemfR = 	BufferA[2][0];	//AN1
-		adc_AN3 = 		BufferA[3][0];	//AN3
+ * Function Name : _DMA0Interrupt
+ * Description   : Interrupt hander for DMA0 , associated with ADC1 here.
+                                  Motor BEMF vales are set through setter functions.
+ * Parameters    : None
+ * Return Value  : None
+ *****************************************************************************/
+void __attribute__((interrupt, no_auto_psv)) _DMA0Interrupt(void) {
+    //This should really be done in an elegant way by selecting a pointer, and
+    // not repeating code below.
+    if (DmaBuffer == 0) {
+        adc_AN0 = BufferA[0][0];  //AN0
+        adc_AN8 = BufferA[1][0];  //AN8
+        adc_AN9 = BufferA[2][0];  //AN9
+        adc_AN10 = BufferA[3][0]; //AN10
+        adc_AN11 = BufferA[4][0]; //AN11
+    } else {
+        adc_AN0 = BufferB[0][0];  //AN0
+        adc_AN8 = BufferB[1][0];  //AN8
+        adc_AN9 = BufferB[2][0];  //AN9
+        adc_AN10 = BufferB[3][0]; //AN10
+        adc_AN11 = BufferB[4][0]; //AN11
+    }
 
-	} else {
+    //Update named variables
+    adc_Vbatt  = adc_AN0;
+    adc_MotorA = adc_AN8;
+    adc_MotorB = adc_AN9;
+    adc_MotorC = adc_AN10;
+    adc_MotorD = adc_AN11;
 
-		adc_bemfL = 	BufferB[0][0];	//AN11
-		adc_battery = 	BufferB[1][0];	//AN0
-		adc_bemfR = 	BufferB[2][0];	//AN1
-		adc_AN3 = 		BufferB[3][0];	//AN3
-	}
-
-	DmaBuffer ^= 1;	 //Toggle between buffers
-	IFS0bits.DMA0IF = 0;		//Clear the DMA0 Interrupt Flag
+    DmaBuffer ^= 1; //Toggle between buffers
+    IFS0bits.DMA0IF = 0; //Clear the DMA0 Interrupt Flag
 }
 // End DMA section
--- lib/adc_pid.h
+++ lib/adc_pid.h
@@ -3,11 +3,19 @@
 
 void adcSetup(void); //Top level config function, to be called from main
 
-//Getters for other modules to access values
-unsigned int adcGetBEMFL();
-unsigned int adcGetBEMFR();
-unsigned int adcGetVBatt();
-unsigned int adcGetAN3();
+//Getters for other modules to access values, by useful name
+unsigned int adcGetVbatt();
+unsigned int adcGetMotorA();
+unsigned int adcGetMotorB();
+unsigned int adcGetMotorC();
+unsigned int adcGetMotorD();
+
+//Getters for other modules to access values, by AN pin name
+unsigned int adcGetAN0();
+unsigned int adcGetAN8();
+unsigned int adcGetAN9();
+unsigned int adcGetAN10();
+unsigned int adcGetAN11();
 
 #ifndef ADC_MAX
 #define ADC_MAX             853
