718da470728fce9c66b9ad4444ef4dac9a0bcd61
Andrew Pullin <pullin@berkeley.edu>
2013-01-17 16:47:34 -0800

    Bringing in ronf's python from attempt to update code base.

--- python/shared.py
+++ python/shared.py
@@ -1,16 +1,12 @@
-
-#Base station
-#BS_COMPORT = 'COM3'
-#BS_BAUDRATE = 230400
-#XBee
 BS_COMPORT = 'COM4'
-BS_BAUDRATE = 57600
+BS_BAUDRATE = 230400
 
-deg2count = 14.375
-count2deg = 1/deg2count
-
-#For download timeout
-last_packet_time = 0
+motor_gains_set = False
+steering_gains_set = False
+steering_rate_set = False
 pkts = 0
+count2deg = 2000.0/(2**15-1)
 
-ROBOTS = []
+imudata = []
+statedata = []
+dutycycles = []
